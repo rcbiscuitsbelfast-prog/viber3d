63d1c40952454bca24608d00e888d2ab6277c018
rcbiscuitsbelfast-prog <r.c.biscuits.belfast@gmail.com>
2026-01-19 15:00:40 +0000

    commit

--- templates/quests4friends/src/App.tsx
+++ templates/quests4friends/src/App.tsx
@@ -8,9 +8,11 @@ import { AccountPage } from './pages/AccountPage';
 import { ToonShooterPage } from './pages/ToonShooterPage';
 import { MinimalDemo } from './pages/MinimalDemoPage';
 import TileWorldDemoPage from './pages/TileWorldDemoPage';
-<<<<<<< HEAD
 import { WorldCanvasBuilderPage } from './pages/WorldCanvasBuilderPage';
-import { TileBuilderPage } from './pages/TileBuilderPage';
+import { TileCreation } from './pages/TileCreationPage';
+import TileEditorPage from './pages/TileEditorPage';
+import WorldBuilderPage from './pages/WorldBuilderPage';
+import WorldFromTilesPage from './pages/WorldFromTilesPage';
 
 export default function App() {
   return (
@@ -26,30 +28,12 @@ export default function App() {
           <Route path="/minimal-demo" element={<MinimalDemo />} />
           <Route path="/tile-world-demo" element={<TileWorldDemoPage />} />
           <Route path="/world-canvas-builder" element={<WorldCanvasBuilderPage />} />
-          <Route path="/tile-builder" element={<TileBuilderPage />} />
+          <Route path="/tile-creation" element={<TileCreation />} />
+          <Route path="/world-from-tiles" element={<WorldFromTilesPage />} />
+          <Route path="/tile-editor" element={<TileEditorPage />} />
+          <Route path="/world-builder" element={<WorldBuilderPage />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
-=======
-import TileEditorPage from './pages/TileEditorPage';
-import WorldBuilderPage from './pages/WorldBuilderPage';
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/play/:questId" element={<QuestPlayerPage />} />
-        <Route path="/builder" element={<BuilderPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/toonshooter" element={<ToonShooterPage />} />
-        <Route path="/minimal-demo" element={<MinimalDemo />} />
-        <Route path="/tile-world-demo" element={<TileWorldDemoPage />} />
-        <Route path="/tile-editor" element={<TileEditorPage />} />
-        <Route path="/world-builder" element={<WorldBuilderPage />} />
-      </Routes>
-    </BrowserRouter>
->>>>>>> 7531c7f9b35571583eae5962519be98a8c206f04
   );
 }
